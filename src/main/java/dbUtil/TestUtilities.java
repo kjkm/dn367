e424b1c78706d484f428a1d138353f161202cbd2
clewarne15@gmail.com <clewarne15@gmail.com>
2022-04-25 19:21:32 -0700

    Added better documentation

--- src/main/java/dbUtil/TestUtilities.java
+++ src/main/java/dbUtil/TestUtilities.java
@@ -14,6 +14,8 @@ public class TestUtilities {
 		while (rs.next()) {
 			System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4));
 		}
+		
+		//test for sessionsByStudent
 		System.out.print("Please input the desired first name of the student: ");
 		String fnameSessionsByStudent = keyboard.nextLine();
 		System.out.print("Please input the desired last name of the student: ");
